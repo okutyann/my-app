8537815a0196388546666eb3056a415bc0115d14
honoka <h19981029@gmail.com>
2022-06-12 10:43:56 +0900

    first

--- src/App.js
+++ src/App.js
@@ -3,16 +3,13 @@ import Todolist from "./Todolist";
 import { v4 as uuidv4 } from "uuid"
 
 function App() {
-  const [todos, setTods] = useState([
-    { id: 1, name: "todo1", completed: true },
-    { id: 2, name: "todo2", completed: false },
-  ]);
-
+  const [todos, setTods] = useState([]);
   const listname = useRef();
 
   //リスト追加
   const listadd = () => {
     const name = listname.current.value;
+    if (name === "") return;
     setTods((prevTodos) => {
       return [...prevTodos, { id: uuidv4(), name: name, completed: false }]
     })
@@ -29,13 +26,18 @@ function App() {
     setTods(newTodos);
   };
 
+  const handlclear = () => {
+    const newTodos = todos.filter((todo) => !todo.completed);
+    setTods(newTodos);
+  };
+
   return (
     <div>
       <Todolist todos={todos} toggleTodo={toggleTodo} />
       <input type="text" ref={listname} />
       <button onClick={listadd}>タスク追加</button>
-      <button>完了したタスクの掃除</button>
-      <div>残りのタスク:0</div>
+      <button onClick={handlclear}>完了したタスクの掃除</button>
+      <div>残りのタスク:{todos.filter((todo) => !todo.completed)}</div>
     </div>
   );
 }
